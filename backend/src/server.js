cd7503b09509b86881b303a1088ad81b60fc8815
heidi_irdrj <heidi.iroudayaradjou@gmail.com>
2026-06-10 15:20:50 +0200

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,25 @@
 import express from "express";
-import { ENV } from "./config/env.js"; // Assure-toi que le chemin est correct
+import { ENV } from "./config/env.js"; 
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { eq, and } from "drizzle-orm";  
 
 const app = express();
 const PORT = ENV.PORT || 5001; 
 
 app.use(express.json());
 
+//  Route Health check
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true });
 });
 
+//  Ajouter un favori
 app.post("/api/favorites", async (req, res) => {
   try {
     const { userId, recipeId, title, image, cookTime, servings } = req.body;
 
-    if (!userId || !recipeId || !title || !image) {
+    if (!userId || !recipeId || !title) {
       return res.status(400).json({ error: "Missing required fields" });
     }
 
@@ -39,6 +42,43 @@ app.post("/api/favorites", async (req, res) => {
   }
 });
 
+// Récupérer les favoris d'un utilisateur
+app.get("/api/favorites/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userFavorites = await db
+      .select()
+      .from(favoritesTable)
+      .where(eq(favoritesTable.userId, userId));
+
+    res.status(200).json(userFavorites);
+  } catch (error) {
+    console.error("Error fetching the favorites:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+//  Supprimer un favori 
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => { 
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favoritesTable)
+      .where(
+        and(
+          eq(favoritesTable.userId, userId), 
+          eq(favoritesTable.recipeId, parseInt(recipeId))
+        )
+      );
+
+    res.status(200).json({ message: "Favorite removed successfully" });
+  } catch (error) {
+    console.error("Error removing a favorite:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
